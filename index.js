2bccd8d2db480951a2838c67a960267ccb8d1e76
Jacob Arvidsson <jacobarvidsson@me.com>
2020-01-16 17:31:20 +0000

    Added a /

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const invalidateCloudFront = ({
         CallerReference: `${+new Date()}`,
         Paths: {
           Quantity: 1,
-          Items: ["*"]
+          Items: ["/*"]
         }
       }
     };
